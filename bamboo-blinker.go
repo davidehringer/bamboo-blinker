1ab2f55270faa1e53680adcb86d901a5276f1faf
Eddie Webb <ollitech@gmail.com>
2015-12-15 12:38:56 -0500

    updated to leverage new goblync library with multi blync support

--- bamboo-blinker.go
+++ bamboo-blinker.go
@@ -12,10 +12,23 @@ import (
 )
 
 func main() {
+
+
+
 	numArgs := len(os.Args)
 	if numArgs < 2 {
-		fmt.Println("Usage: bamboo-blinker URL [INTERVAL_SECONDS] [BACKOFF_MS]")
-		os.Exit(1)
+			fmt.Println("Usage: bamboo-blinker URL [INTERVAL_SECONDS] [BACKOFF_MS] [BLYNC_ID]")
+			fmt.Printf("\t This will start monitoring the provided URL using a specific BlyncLight, default to ID 1\n")
+			fmt.Println("OR : bamboo-blinker TellMeHowItWillBe")
+			fmt.Printf("\t This will flash all connect blync lights in logical ID order based on USB port\n")
+			os.Exit(1)
+	}
+
+	light := blync.NewBlyncLight()
+
+	if os.Args[1] == "FlashMob"{
+			light.FlashOrder()
+			os.Exit(1)
 	}
 	url := os.Args[1]
 	buildBunny := bunny.NewBunny(url)
@@ -41,10 +54,18 @@ func main() {
 		backoffLimit = int(value)
 	}
 
+	blyncId := 1
+	if numArgs == 5 {
+		value, err := strconv.ParseInt(os.Args[4], 10, 32)
+		if err != nil {
+			fmt.Println("BLYNC_ID must be an integer")
+			os.Exit(1)
+		}
+		blyncId = int(value)
+	}
 
 
-	light := blync.NewBlyncLight()
-	light.SetColor(blync.Green)
+	light.SetColor(blync.Green, blyncId)
 
 	// clean shutdown of light
 	c := make(chan os.Signal, 1)
@@ -52,11 +73,11 @@ func main() {
 	signal.Notify(c, syscall.SIGTERM)
 	go func() {
 		<-c
-		light.Close()
+		light.Close(blyncId)
 		os.Exit(1)
 	}()
 
-	monitor := NewMonitor(light)
+	monitor := NewMonitor(light, blyncId)
 
 	for {
 		bunnyStatus := buildBunny.Update()
@@ -88,24 +109,26 @@ func main() {
 type monitor struct {
 	healthy bool
 	light blync.BlyncLight
+	blyncId int
 }
 
-func NewMonitor(light blync.BlyncLight) (m monitor) {
+func NewMonitor(light blync.BlyncLight, blyncId int) (m monitor) {
 	m.healthy = true;
 	m.light = light
+	m.blyncId = blyncId
 	return
 }
 
 func (m *monitor) SetHealthy() {
 	if !m.healthy {
 		m.healthy = true
-		m.light.StopPlay()
-		m.light.SetBlinkRate(blync.BlinkOff)
+		m.light.StopPlay(m.blyncId)
+		m.light.SetBlinkRate(blync.BlinkOff, m.blyncId)
 		for i := 0; i < 256; i++ {
-			m.light.SetColor([3]byte{255 - byte(i), byte(i), 0x00})
+			m.light.SetColor([3]byte{255 - byte(i), byte(i), 0x00}, m.blyncId)
 			time.Sleep(13 * time.Millisecond)
 		}
-		m.light.Play(28)
+		m.light.Play(28, m.blyncId)
 	}
 }
 
@@ -113,14 +136,14 @@ func (m *monitor) SetUnhealthy() {
 	if m.healthy {
 		m.healthy = false
 		for i := 0; i < 256; i++ {
-			m.light.SetColor([3]byte{byte(i), 255 - byte(i), 0x00})
+			m.light.SetColor([3]byte{byte(i), 255 - byte(i), 0x00}, m.blyncId)
 			time.Sleep(13 * time.Millisecond)
 		}
-		m.light.SetBlinkRate(blync.BlinkMedium)
-		m.light.Play(52)
+		m.light.SetBlinkRate(blync.BlinkMedium, m.blyncId)
+		m.light.Play(52, m.blyncId)
 		// We using a never ending sound because it was one of the only ones that 
 		// had some sound of urgency to it.  But we don't want it to keep playing
 		time.Sleep(time.Second * 15)
-		m.light.StopPlay()
+		m.light.StopPlay(m.blyncId)
 	}
 }
